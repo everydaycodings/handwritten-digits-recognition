75d2735500ad886dbe5f6c3c3b3cdf51a60946e6
Kumar Saksham <everydaycodings@gmail.com>
2020-11-26 14:08:34 +0530

    added the user destination input

--- handwritten_digits_recognition.py
+++ handwritten_digits_recognition.py
@@ -46,16 +46,15 @@ else:
     model = tf.keras.models.load_model('handwritten_digits.model')
 
 # Load custom images and predict them
-image_number = 1
-while os.path.isfile('digits/digit{}.png'.format(image_number)):
-    try:
-        img = cv2.imread('digits/digit{}.png'.format(image_number))[:,:,0]
-        img = np.invert(np.array([img]))
-        prediction = model.predict(img)
-        print("The number is probably a {}".format(np.argmax(prediction)))
-        plt.imshow(img[0], cmap=plt.cm.binary)
-        plt.show()
-        image_number += 1
-    except:
-        print("Error reading image! Proceeding with next image...")
-        image_number += 1
+user = input("Enter the Destination of the Image: ")
+try:
+    img = cv2.imread(user)[:,:,0]
+    img = np.invert(np.array([img]))
+    prediction = model.predict(img)
+    print("The number is probably a {}".format(np.argmax(prediction)))
+    plt.imshow(img[0], cmap=plt.cm.binary)
+    plt.show()
+
+except:
+    print("Error reading image! Proceeding with next image...")
+
